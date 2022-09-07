a0d207872a51cfef03ef8d3bee965f69671d83ab
Tarik El shaar <ihorgosha9@gmail.com>
2022-09-07 20:04:14 +0300

    labels fix

--- src/pages/inner-pages/Labels.js
+++ src/pages/inner-pages/Labels.js
@@ -52,8 +52,219 @@ const labelsGreen = [
         text: "Label 1",
         variant: "green",
     },
+    {
+        id: 8,
+        icon: <StarSmall color="#027A48" />,
+        text: "Label 1",
+        variant: "green",
+    },
 
 ];
+
+
+const labelsRed = [
+    {
+        id: 1,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 2,
+        icon: <CheckSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 3,
+        icon: <DotsSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 4,
+        icon: <PlaneSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 5,
+        icon: <CircleSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 6,
+        icon: <AlertSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 7,
+        icon: <CloseSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+    {
+        id: 8,
+        icon: <StarSmall color="#B42318" />,
+        text: "Label 2",
+        variant: "red",
+    },
+
+];
+
+const labelsGrey = [
+    {
+        id: 1,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 2,
+        icon: <CheckSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 3,
+        icon: <DotsSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 4,
+        icon: <PlaneSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 5,
+        icon: <CircleSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 6,
+        icon: <AlertSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 7,
+        icon: <CloseSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+    {
+        id: 8,
+        icon: <StarSmall color="#344054" />,
+        text: "Label 3",
+        variant: "grey",
+    },
+
+];
+
+const labelsBlue = [
+    {
+        id: 1,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 2,
+        icon: <CheckSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 3,
+        icon: <DotsSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 4,
+        icon: <PlaneSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 5,
+        icon: <CircleSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 6,
+        icon: <AlertSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 7,
+        icon: <CloseSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+    {
+        id: 8,
+        icon: <StarSmall color="#1D5AC5" />,
+        text: "Label 4",
+        variant: "blue",
+    },
+
+];
+
+const labelsOrange = [
+    {
+        id: 1,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 2,
+        icon: <CheckSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 3,
+        icon: <DotsSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 4,
+        icon: <PlaneSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 5,
+        icon: <CircleSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 6,
+        icon: <AlertSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 7,
+        icon: <CloseSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+    {
+        id: 8,
+        icon: <StarSmall color="#B54708" />,
+        text: "Label 5",
+        variant: "orange",
+    },
+];
+
 
 const Labels = () => {
     return (
@@ -61,11 +272,10 @@ const Labels = () => {
             <SHeader>
                 <h5>Components</h5>
                 <h1>Labels</h1>
-                <p>Lorem ipsum dolor sim amet</p>
+                <p>The label component allows users to add specific element captions for user clarity and convenience.</p>
             </SHeader>
 
-            <SComponentCage direction="column">
-
+            <SComponentCage>
                 <SSizeControl>
                     {labelsGreen.map((card) => (
                         <Label
@@ -74,29 +284,50 @@ const Labels = () => {
                         />
                     ))}
                 </SSizeControl>
+            </SComponentCage>
+
+            <SComponentCage>
                 <SSizeControl>
-                    {labelsGreen.map((card) => (
+                    {labelsRed.map((card) => (
                         <Label
                             key={card.id}
                             {...card}
                         />
                     ))}
                 </SSizeControl>
+            </SComponentCage>
 
-                {/*<SSizeControl style={{maxWidth:'100%'}}>*/}
-                {/*    <Label text="Label 2" variant="red" icon={<CloseSmall />} />*/}
-                {/*    <Label text="Label 2" variant="red"/>*/}
-                {/*</SSizeControl>*/}
-                {/*<SSizeControl style={{maxWidth:'100%'}}>*/}
-                {/*    <Label text="Label 2" variant="red" icon={<CloseSmall />} />*/}
-                {/*    <Label text="Label 2" variant="red"/>*/}
-                {/*    <Label text="Label 3" variant="grey" icon={<CircleSmall />} />*/}
-                {/*    <Label text="Label 3" variant="grey"/>*/}
-                {/*    <Label text="Label 4" variant="blue" icon={<StarSmall />} />*/}
-                {/*    <Label text="Label 4" variant="blue"/>*/}
-                {/*    <Label text="Label 5" variant="orange" icon={<AlertSmall />} />*/}
-                {/*    <Label text="Label 5" variant="orange"/>*/}
-                {/*</SSizeControl>*/}
+            <SComponentCage>
+                <SSizeControl>
+                    {labelsGrey.map((card) => (
+                        <Label
+                            key={card.id}
+                            {...card}
+                        />
+                    ))}
+                </SSizeControl>
+            </SComponentCage>
+
+            <SComponentCage>
+                <SSizeControl>
+                    {labelsBlue.map((card) => (
+                        <Label
+                            key={card.id}
+                            {...card}
+                        />
+                    ))}
+                </SSizeControl>
+            </SComponentCage>
+
+            <SComponentCage>
+                <SSizeControl>
+                    {labelsOrange.map((card) => (
+                        <Label
+                            key={card.id}
+                            {...card}
+                        />
+                    ))}
+                </SSizeControl>
             </SComponentCage>
 
         </>
