28b40fd29c5fbd43e7c049760a9f597e2b6830cd
Tarik El shaar <ihorgosha9@gmail.com>
2022-10-10 03:33:03 +0300

    job card

--- src/components/System/JobCard/JobCard.js
+++ src/components/System/JobCard/JobCard.js
@@ -1,23 +1,65 @@
 import React from "react";
-import {SJobCard, SJobCardDetails, SJobCardTopPart} from "./styles";
+import {
+    SDateInfo,
+    SDecorationBar,
+    SJobCard,
+    SJobCardBottomPart,
+    SJobCardDetails,
+    SJobCardTopPart,
+    SSmallInfoBlock
+} from "./styles";
 import CircleJobDetails from "../../../assets/icons/CircleJobDetails";
+import CompanyLogo from "../../../assets/company-logo/jet-aviation.svg";
+import TextTruncate from 'react-text-truncate';
 
 const JobCard = ({title}) => {
     return (
         <SJobCard>
+
+            <SDecorationBar />
+
             <SJobCardTopPart>
+                <SDateInfo>
+                    <p>Posted on 05/25/2022</p>
+                    <img src={CompanyLogo} alt="React Logo" />
+                </SDateInfo>
+
                 <h2>{title}</h2>
                 <SJobCardDetails>
-                    <li><span>$800 — 1.2k per day</span></li>
-                    <CircleJobDetails />
+                    <li>$800 — 1.2k per day</li>
+                    <li><span>/</span></li>
                     <li>KPDX — Portland International Airports</li>
-                    <CircleJobDetails />
+                    <li><span>/</span></li>
                     <li>5 years of experience</li>
-                    <CircleJobDetails />
+                    <li><span>/</span></li>
                     <li>Full-time</li>
                 </SJobCardDetails>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud aliquip ex ea commodo consequat.</p>
             </SJobCardTopPart>
+
+            <SJobCardBottomPart>
+                <SSmallInfoBlock>
+                    <p>Starting point</p>
+                    <span>KPDX — 04/20/2022</span>
+                </SSmallInfoBlock>
+                <SSmallInfoBlock>
+                    <p>Ending point</p>
+                    <span>KDAL — 04/20/2022</span>
+                </SSmallInfoBlock>
+                <SSmallInfoBlock>
+                    <p>Route</p>
+                    <span>KPDX ⇢ KDAL ⇢ KSTL</span>
+                </SSmallInfoBlock>
+                <SSmallInfoBlock>
+                    <p>Aircraft type</p>
+                    <TextTruncate
+                        line={1}
+                        element="span"
+                        truncateText="…"
+                        text="Cessna 525, Gulfstream G650, Gulfstream G700, Gulfstream G650"
+                    />
+                </SSmallInfoBlock>
+            </SJobCardBottomPart>
         </SJobCard>
     );
 };
--- src/components/System/JobCard/styles.js
+++ src/components/System/JobCard/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const SJobCard = styled.div`
-  height: 300px;
+  position: relative;
   width: 100%;
   background: white;
   border-radius: 10px;
@@ -18,6 +18,9 @@ export const SJobCard = styled.div`
 
 export const SJobCardTopPart = styled.div`
   padding: 24px;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid #eaecf0;
   
   h2 {
     font-family: 'Inter';
@@ -41,11 +44,89 @@ export const SJobCardDetails = styled.ul`
   display: flex;
   flex-direction: row;
   align-items: center;
-  gap: 8px;
+  gap: 16px;
   padding: 0px;
   list-style-type: none;
+  margin-top: 24px;
+  margin-bottom: 8px;
+  
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  color: #202121;
   
   span {
+    font-weight: 400;
+    color: #1EAAE2;
+  }
+`;
+
+export const SDateInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+  height: 20px;
+  
+  p {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: #667085;
+  }
+  
+  img {
+    position: absolute;
+    right: 24px;
+  }
+  
+`;
+
+export const SDecorationBar = styled.div`
+  background: linear-gradient(90deg, #1B3A6B 0%, #446EBD 100%);
+  height: 4px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin-right: 24px;
+  margin-left: 24px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  z-index: 50;
+`;
+export const SJobCardBottomPart = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 24px;
+`;
+export const SSmallInfoBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+  
+  
+  span {
+    color: #202121;
+    font-family: 'Inter';
+    font-style: normal;
     font-weight: 600;
+    font-size: 14px;
+    line-height: 20px;
+    }
+  p {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    color: #667085;
   }
 `;
